e180756d1cdcd48589ed92bec9786fc61edbf894
sw-sys <sheldonkorpet@gmail.com>
2024-04-19 21:57:05 +0100

    update saving options on spiral generator

--- turtle/test_pattern_gen_spiral.py
+++ turtle/test_pattern_gen_spiral.py
@@ -1,24 +1,37 @@
 from turtle import *
+from turtle2img import save_jpg, save_png
+
+### color schemes
+# rainbow '#FF6D6A', '#EFBE7D','#E9EC6B','#C1F0B2', '#8BD3E6', '#B1A2CA'
+# blues '#95C8D8', '#B0E0E6', '#8AB9F1', '#034694', '#0F52BA', '#000080', '#003151'
+# neutrals '#FDE992', '#F4A460', '#E8CAA2', '#D1AF90', '#DCD7A0', '#B37959', '#856D4D'
 
 # cursor settings
+hideturtle()
 speed(0)
-width(15)
+width(10)
 
 # bkgd settings
-Screen().bgcolor("black")
+Screen().bgcolor("white")
 
 # drawing
 for steps in range(250):
-    for c in ('white', 'black'):
+    for c in ('#FDE992', '#F4A460', '#E8CAA2', '#D1AF90', '#DCD7A0', '#B37959', '#856D4D'):
         color(c)
         forward(steps)
         right(15)
 
-# save to image
+### save image - test
 def save(file_name):
-    Screen().getcanvas().postscript(file=file_name, colormode='color')
+    save_png(file_name + ".png")
 
-save("turtle_drawing_15-degrees_250-turns_WIDTH15_bkg-black_linesW.eps")
+save(r"spirals\turtle_spiral_neutral_fossil_gradient_15_250")
+
+# # save to image - colour
+# def save(file_name):
+#     Screen().getcanvas().postscript(file=file_name, colormode='color')
+
+# save(r"spirals\turtle_spiral_neutral_gradient_.eps")
 
 # sustain window
 mainloop()
